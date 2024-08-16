32e8bd4ae6dca1294aef48727d235f0426ce1d32
Patryk Małek <patryk.malek@konghq.com>
2024-08-16 10:37:40 +0200

    feat: add ExtractTags

---
+++ pkg/metadata/consts.go
@@ -0,0 +1,9 @@
+package metadata
+
+const (
+	// AnnotationPrefix is the prefix for Kong annotations.
+	AnnotationPrefix = "konghq.com"
+
+	// TagKey is the key for the tags annotation.
+	TagKey = "/tags"
+)
