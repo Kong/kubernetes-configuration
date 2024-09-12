011c949420ade8781f1024c4297442ff47b0ce11
Yi Tao <tao.yi@konghq.com>
2024-09-12 15:36:22 +0800

    add KongTarget CRD

--- pkg/clientset/typed/configuration/v1alpha1/configuration_client.go
+++ pkg/clientset/typed/configuration/v1alpha1/configuration_client.go
@@ -34,6 +34,7 @@ type ConfigurationV1alpha1Interface interface {
 	KongPluginBindingsGetter
 	KongRoutesGetter
 	KongServicesGetter
+	KongTargetsGetter
 	KongUpstreamsGetter
 	KongVaultsGetter
 }
@@ -65,6 +66,10 @@ func (c *ConfigurationV1alpha1Client) KongRoutes(namespace string) KongRouteInte
 
 func (c *ConfigurationV1alpha1Client) KongServices(namespace string) KongServiceInterface {
 	return newKongServices(c, namespace)
+}
+
+func (c *ConfigurationV1alpha1Client) KongTargets(namespace string) KongTargetInterface {
+	return newKongTargets(c, namespace)
 }
 
 func (c *ConfigurationV1alpha1Client) KongUpstreams(namespace string) KongUpstreamInterface {
