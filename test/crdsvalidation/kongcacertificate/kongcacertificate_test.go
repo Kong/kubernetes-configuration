157ce9cbd3a96b492b3e895a8ae3f4ad91bd1d7d
Grzegorz Burzyński <czeslavo@gmail.com>
2024-09-18 08:23:11 +0200

    lint

--- test/crdsvalidation/kongcacertificate/kongcacertificate_test.go
+++ test/crdsvalidation/kongcacertificate/kongcacertificate_test.go
@@ -22,10 +22,8 @@ func TestKongCACertificate(t *testing.T) {
 	require.NoError(t, err, "error creating configurationv1alpha1 client")
 
 	for _, tcsGroup := range testcases.TestCases {
-		tcsGroup := tcsGroup
 		t.Run(tcsGroup.Name, func(t *testing.T) {
 			for _, tc := range tcsGroup.TestCases {
-				tc := tc
 				t.Run(tc.Name, func(t *testing.T) {
 					cl := cl.KongCACertificates(tc.KongCACertificate.Namespace)
 					entity, err := cl.Create(ctx, &tc.KongCACertificate, metav1.CreateOptions{})
