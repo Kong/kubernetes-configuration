977651496ab19614deadb228d110509f6bc1682e
Patryk Małek <patryk.malek@konghq.com>
2025-02-06 12:04:59 +0100

    chore: add cp ref enum const aliases

--- api/configuration/v1alpha1/controlplaneref_types.go
+++ api/configuration/v1alpha1/controlplaneref_types.go
@@ -11,6 +11,14 @@ import "github.com/kong/kubernetes-configuration/api/configuration/common"
 type ControlPlaneRef common.ControlPlaneRef
 
 const (
+	// ControlPlaneRefKonnectID is the type for the KonnectID ControlPlaneRef.
+	// It is used to reference a Konnect Control Plane entity by its ID on the Konnect platform.
+	ControlPlaneRefKonnectID = common.ControlPlaneRefKonnectID
 	// ControlPlaneRefKonnectNamespacedRef is the type for the KonnectNamespacedRef ControlPlaneRef.
+	// It is used to reference a Konnect Control Plane entity inside the cluster
+	// using a namespaced reference.
 	ControlPlaneRefKonnectNamespacedRef = common.ControlPlaneRefKonnectNamespacedRef
+	// ControlPlaneRefKIC is the type for KIC ControlPlaneRef.
+	// It is used to reference a KIC as Control Plane.
+	ControlPlaneRefKIC = common.ControlPlaneRefKIC
 )
