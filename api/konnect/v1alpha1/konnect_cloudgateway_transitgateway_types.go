9d684eb8bc0b315f9101d3dc93894f9a61860c70
Yi Tao <tao.yi@konghq.com>
2025-04-03 09:35:07 +0800

    split config for different types of transit gateways

--- api/konnect/v1alpha1/konnect_cloudgateway_transitgateway_types.go
+++ api/konnect/v1alpha1/konnect_cloudgateway_transitgateway_types.go
@@ -36,67 +36,118 @@ type KonnectCloudGatewayTransitGateway struct {
 
 // KonnectCloudGatewayTransitGatewaySpec defines the desired state of KonnectCloudGatewayTransitGateway.
 //
-// +kubebuilder:validation:XValidation:rule="self.type == oldSelf.type",message="spec.type is immutable"
-// +kubebuilder:validation:XValidation:rule="self.type == 'AWS Transit Gateway' ? self.attachment_config.kind == 'aws-transit-gateway-attachment' : true",message="AWS transit gateway must have spec.attachment_config.kind set to 'aws-transit-gateway-attachment'"
-// +kubebuilder:validation:XValidation:rule="self.type == 'AWS AWS VPC Peering Gateway' ? self.attachment_config.kind == 'aws-vpc-peering-attachment' : true",message="AWS VPC peering gateway must have spec.attachment_config.kind set to 'aws-vpc-peering-attachment'"
-// +kubebuilder:validation:XValidation:rule="self.type == 'Azure Transit Gateway' ? self.attachment_config.kind == 'azure-vnet-peering-attachment' : true",message="Azure VNET peering gateway must have spec.attachment_config.kind set to 'azure-vnet-peering-attachment'"
+// +kubebuilder:validation:XValidation:rule="self.type == oldSelf.type", message="spec.type is immutable"
+// +kubebuilder:validation:XValidation:rule="self.type == 'AWS Transit Gateway' ? has(self.awsTransitGateway) : true", message="must set spec.awsTransitGateway when spec.type is 'AWS Transit Gateway'"
+// +kubebuilder:validation:XValidation:rule="self.type != 'AWS Transit Gateway' ? !has(self.awsTransitGateway) : true", message="must not set spec.awsTransitGateway when spec.type is not 'AWS Transit Gateway'"
+// +kubebuilder:validation:XValidation:rule="self.type == 'AWS VPC Peering Gateway' ? has(self.awsVPCPeeringGateway) : true", message = "must set spec.awsVPCPeeringGateway when spec.type is 'AWS VPC Peering Gateway'"
+// +kubebuilder:validation:XValidation:rule="self.type != 'AWS VPC Peering Gateway' ? !has(self.awsVPCPeeringGateway) : true", message = "must not set spec.awsVPCPeeringGateway when spec.type is not 'AWS VPC Peering Gateway'"
+// +kubebuilder:validation:XValidation:rule="self.type == 'Azure Transit Gateway' ? has(self.azureTransitGateway) : true", message = "must set spec.azureTransitGateway when spec.type is 'Azure Transit Gateway'"
+// +kubebuilder:validation:XValidation:rule="self.type != 'Azure Transit Gateway' ? !has(self.azureTransitGateway) : true", message = "must not set spec.azureTransitGateway when spec.type is not 'Azure Transit Gateway'"
+// +kubebuilder:validation:XValidation:rule="self.type == 'AWS Transit Gateway' ? (has(self.awsTransitGateway) && self.awsTransitGateway.attachment_config.kind == 'aws-transit-gateway-attachment') : true",message="must set spec.awsTransitGateway.attachment_config.kind to 'aws-transit-gateway-attachment' for AWS transit gateway type"
+// +kubebuilder:validation:XValidation:rule="self.type == 'AWS VPC Peering Gateway' ? (has(self.awsVPCPeeringGateway) && self.awsVPCPeeringGateway.attachment_config.kind == 'aws-vpc-peering-attachment') : true",message="must set spec.awsVPCPeeringGateway.attachment_config.kind to 'aws-vpc-peering-attachment' for AWS VPC peering gateway type"
+// +kubebuilder:validation:XValidation:rule="self.type == 'Azure Transit Gateway' ? (has(self.azureTransitGateway) && self.azureTransitGateway.attachment_config.kind == 'azure-vnet-peering-attachment') : true",message="must set spec.azureTransitGateway.attachment_config.kind to 'azure-vnet-peering-attachment' for Azure transit gateway type"
 // TODO: add more constraints on attachment_config based on type
 type KonnectCloudGatewayTransitGatewaySpec struct {
 	// NetworkRef is the schema for the NetworkRef type.
 	//
 	// +kubebuilder:validation:Required
 	NetworkRef commonv1alpha1.ObjectRef `json:"networkRef"`
-
+	// KonnectTransitGatewayAPISpec is the configuration of the transit gateway on Konnect side.
 	KonnectTransitGatewayAPISpec `json:",inline"`
 }
 
 // KonnectTransitGatewayAPISpec specifies a transit gateway on the Konnect side.
 // The type and all the types it referenced are mostly copied github.com/Kong/sdk-konnect-go/models/components.CreateTransitGatewayRequest.
-// We consolidated the common fields other than 'attachment_config' because k8s CRDs does not accept slices with the same json keys in different types.
 //
 // TODO: add necessary comments for types/fields if they were missing in sdk-konnect-go.
 type KonnectTransitGatewayAPISpec struct {
 	// Type is the type of the Konnect transit gateway.
-	// Currently types AWS Transit Gateway, AWS VPC Peering Gateway, Azure Transit Gateway are supported.
 	//
+	// +kubebuilder:validation:Required
 	// +kubebuilder:validation:Enum=AWS Transit Gateway;AWS VPC Peering Gateway;Azure Transit Gateway
 	Type sdkkonnectcomp.CreateTransitGatewayRequestType `json:"type"`
-	// Human-readable name of the transit gateway.
-	Name string `json:"name"`
-	// List of mappings from remote DNS server IP address sets to proxied internal domains, for a transit gateway
-	// attachment.
-	//
-	DNSConfig []TransitGatewayDNSConfig `json:"dns_config,omitempty"`
-	// CIDR blocks for constructing a route table for the transit gateway, when attaching to the owning
-	// network.
-	//
-	CIDRBlocks []string `json:"cidr_blocks,omitempty"`
+
 	// AWSTransitGateway is the configuration of an AWS transit gateway.
 	// Used when type is "AWS Transit Gateway".
-	AWSTransitGateway *AWSTransitGateway `json:",inline,omitempty"`
+	//
+	// +kubebuilder:validation:Optional
+	AWSTransitGateway *AWSTransitGateway `json:"awsTransitGateway,omitempty"`
 	// AWSVPCPeeringGateway is the configuration of an AWS VPC peering gateway.
 	// Used when type is "AWS VPC Peering Gateway".
-	AWSVPCPeeringGateway *AWSVPCPeeringGateway `json:",inline,omitempty"`
+	//
+	// +kubebuilder:validation:Optional
+	AWSVPCPeeringGateway *AWSVPCPeeringGateway `json:"awsVPCPeeringGateway,omitempty"`
 	// AzureTransitGateway is the configuration of an Azure transit gateway.
 	// Used when type is "Azure Transit Gateway".
-	AzureTransitGateway *AzureTransitGateway `json:",inline,omitempty"`
+	//
+	// +kubebuilder:validation:Optional
+	AzureTransitGateway *AzureTransitGateway `json:"azureTransitGateway,omitempty"`
 }
 
 // AWSTransitGateway is the configuration of an AWS transit gateway.
 type AWSTransitGateway struct {
+	// Human-readable name of the transit gateway.
+	//
+	// +kubebuilder:validation:MinLength=1
+	// +kubebuilder:validation:MaxLength=120
+	// +kubebuilder:validation:Required
+	Name string `json:"name"`
+	// List of mappings from remote DNS server IP address sets to proxied internal domains, for a transit gateway
+	// attachment.
+	//
+	// +kubebuilder:validation:Optional
+	DNSConfig []TransitGatewayDNSConfig `json:"dns_config,omitempty"`
+	// CIDR blocks for constructing a route table for the transit gateway, when attaching to the owning
+	// network.
+	//
+	// +kubebuilder:validation:Required
+	CIDRBlocks []string `json:"cidr_blocks"`
 	// configuration to attach to AWS transit gateway on the AWS side.
+	//
+	// +kubebuilder:validation:Required
 	AttachmentConfig AwsTransitGatewayAttachmentConfig `json:"attachment_config"`
 }
 
 // AWSVPCPeeringGateway is the configuration of an AWS VPC peering gateway.
 type AWSVPCPeeringGateway struct {
-	// configuartion to attach to AWS VPC peering gateway on AWS side.
+	// Human-readable name of the transit gateway.
+	//
+	// +kubebuilder:validation:MinLength=1
+	// +kubebuilder:validation:MaxLength=120
+	// +kubebuilder:validation:Required
+	Name string `json:"name"`
+	// List of mappings from remote DNS server IP address sets to proxied internal domains, for a transit gateway
+	// attachment.
+	//
+	// +kubebuilder:validation:Optional
+	DNSConfig []TransitGatewayDNSConfig `json:"dns_config,omitempty"`
+	// CIDR blocks for constructing a route table for the transit gateway, when attaching to the owning
+	// network.
+	//
+	// +kubebuilder:validation:Required
+	CIDRBlocks []string `json:"cidr_blocks"`
+	// configuration to attach to AWS VPC peering gateway on AWS side.
+	//
+	// +kubebuilder:validation:Required
 	AttachmentConfig AwsVpcPeeringGatewayAttachmentConfig `json:"attachment_config"`
 }
 
 // AzureTransitGateway is the configuration of an Azure transit gateway.
 type AzureTransitGateway struct {
-	// configuartion to attach to Azure VNET peering gateway.
+	// Human-readable name of the transit gateway.
+	//
+	// +kubebuilder:validation:MinLength=1
+	// +kubebuilder:validation:MaxLength=120
+	// +kubebuilder:validation:Required
+	Name string `json:"name"`
+	// List of mappings from remote DNS server IP address sets to proxied internal domains, for a transit gateway
+	// attachment.
+	//
+	// +kubebuilder:validation:Optional
+	DNSConfig []TransitGatewayDNSConfig `json:"dns_config,omitempty"`
+	// configuration to attach to Azure VNET peering gateway.
+	//
+	// +kubebuilder:validation:Required
 	AttachmentConfig AzureVNETPeeringAttachmentConfig `json:"attachment_config"`
 }
 
@@ -111,32 +162,49 @@ type TransitGatewayDNSConfig struct {
 
 // AwsTransitGatewayAttachmentConfig is the configuration to attach to a AWS transit gateway.
 type AwsTransitGatewayAttachmentConfig struct {
+	// +kubebuilder:validation:Required
 	Kind sdkkonnectcomp.AWSTransitGatewayAttachmentType `json:"kind"`
 	// AWS Transit Gateway ID to create attachment to.
-	TransitGatewayID string `json:"transit_gateway_id,omitempty"`
+	//
+	// +kubebuilder:validation:Required
+	TransitGatewayID string `json:"transit_gateway_id"`
 	// Resource Share ARN to verify request to create transit gateway attachment.
-	RAMShareArn string `json:"ram_share_arn,omitempty"`
+	//
+	// +kubebuilder:validation:Required
+	RAMShareArn string `json:"ram_share_arn"`
 }
 
 // AwsVpcPeeringGatewayAttachmentConfig is the configuration to attach to a AWS VPC peering gateway.
 type AwsVpcPeeringGatewayAttachmentConfig struct {
-	Kind          sdkkonnectcomp.AWSVPCPeeringAttachmentConfig `json:"kind"`
-	PeerAccountID string                                       `json:"peer_account_id,omitempty"`
-	PeerVpcID     string                                       `json:"peer_vpc_id,omitempty"`
-	PeerVpcRegion string                                       `json:"peer_vpc_region,omitempty"`
+	// +kubebuilder:validation:Required
+	Kind sdkkonnectcomp.AWSVPCPeeringAttachmentConfig `json:"kind"`
+	// +kubebuilder:validation:Required
+	PeerAccountID string `json:"peer_account_id"`
+	// +kubebuilder:validation:Required
+	PeerVpcID string `json:"peer_vpc_id"`
+	// +kubebuilder:validation:Required
+	PeerVpcRegion string `json:"peer_vpc_region"`
 }
 
 // AzureVNETPeeringAttachmentConfig is the configuration to attach to a Azure VNET peering gateway.
 type AzureVNETPeeringAttachmentConfig struct {
 	Kind sdkkonnectcomp.AzureVNETPeeringAttachmentType `json:"kind"`
 	// Tenant ID for the Azure VNET Peering attachment.
-	TenantID string `json:"tenant_id,omitempty"`
+	//
+	// +kubebuilder:validation:Required
+	TenantID string `json:"tenant_id"`
 	// Subscription ID for the Azure VNET Peering attachment.
-	SubscriptionID string `json:"subscription_id,omitempty"`
+	//
+	// +kubebuilder:validation:Required
+	SubscriptionID string `json:"subscription_id"`
 	// Resource Group Name for the Azure VNET Peering attachment.
-	ResourceGroupName string `json:"resource_group_name,omitempty"`
+	//
+	// +kubebuilder:validation:Required
+	ResourceGroupName string `json:"resource_group_name"`
 	// VNET Name for the Azure VNET Peering attachment.
-	VnetName string `json:"vnet_name,omitempty"`
+	//
+	// +kubebuilder:validation:Required
+	VnetName string `json:"vnet_name"`
 }
 
 // KonnectCloudGatewayTransitGatewayStatus defines the current state of KonnectCloudGatewayTransitGateway.
