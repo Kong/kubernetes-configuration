1a1ee35ca9391e423a5377695333e02dcf8d81e0
Yi Tao <tao.yi@konghq.com>
2025-08-13 17:09:30 +0800

    add controlPlaneOptionsValid condition type and reason

--- api/gateway-operator/controlplane/conditions.go
+++ api/gateway-operator/controlplane/conditions.go
@@ -15,6 +15,10 @@ const (
 	// indicate whether or not the ControlPlane has been granted permission to
 	// watch resources in the requested namespaces.
 	ConditionTypeWatchNamespaceGrantValid consts.ConditionType = "WatchNamespaceGrantValid"
+
+	// ConditionTypeOptionsValid is a condition type used to indicate whether or not
+	// the ControlPlane's options is valid by the checks of the operator.
+	ConditionTypeOptionsValid consts.ConditionType = "OptionsValid"
 )
 
 // -----------------------------------------------------------------------------
@@ -50,4 +54,12 @@ const (
 	// WatchNamespaceGrants are valid for the ControlPlane to be able to watch
 	// resources in requested namespaces.
 	ConditionReasonWatchNamespaceGrantValid consts.ConditionReason = "WatchNamespaceGrantValid"
+
+	// ConditionReasonOptionsValid is a reason which indicates that the options
+	// of the ControlPlane is valid with the check of the operator.
+	ConditionReasonOptionsValid consts.ConditionReason = "OptionsValid"
+
+	// ConditionReasonOptionsInvalid is a reason which indicates that the options
+	// of the ControlPlane is not valid with the check of the operator.
+	ConditionReasonOptionsInvalid consts.ConditionReason = "OptionsInvalid"
 )
