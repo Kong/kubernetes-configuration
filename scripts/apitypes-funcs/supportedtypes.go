8981c6e9428cefc92ec4adc23246b5e273cc7a25
Jakub Warczarek <jakub.warczarek@konghq.com>
2025-08-25 11:11:35 +0200

    chore: deduplicate KIC and KGO CRDs and remove deprecated `gateway-operator.konghq.com` `KonnectExtension`

--- scripts/apitypes-funcs/supportedtypes.go
+++ scripts/apitypes-funcs/supportedtypes.go
@@ -245,14 +245,6 @@ var supportedGatewayOperatorTypes = []supportedTypesT{
 		},
 	},
 	{
-		PackageVersion: "v1alpha1",
-		Types: []templateDataT{
-			{
-				Type: "KonnectExtension",
-			},
-		},
-	},
-	{
 		PackageVersion: "v1beta1",
 		Types: []templateDataT{
 			{
